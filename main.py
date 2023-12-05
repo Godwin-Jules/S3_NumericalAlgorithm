5393e55bcf254514b5c368d320bda19b7865e521
@KJules.Developer <mdjulesdeveloper23@gmail.com>
2023-12-05 14:18:48 +0000

    Adding of the main file's content informations

---
+++ main.py
@@ -0,0 +1,8 @@
+"""
+    Author : Jules Dieu donné
+    Date :
+    Motif : the main file of the whole project
+    Inputs : all the information we need to make this project work successfull
+    Ouputs : the outputs of the each function
+"""
+
