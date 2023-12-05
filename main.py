77bbf9330f0e306ae173f9df5a627212ddae7034
@KJules.Developer <mdjulesdeveloper23@gmail.com>
2023-12-05 14:40:57 +0000

    The first correct code

--- main.py
+++ main.py
@@ -6,3 +6,23 @@
     Ouputs : the outputs of the each function
 """
 
+print( "\t\t\t+-----------------------------------------+" )
+print( "\t\t\t|         LES PROGRAMMES DE MTH_300       |" )
+print( "\t\t\t+-----------------------------------------+\n" )
+
+print( "\t\t_________________________ MENU _________________________\n" )
+print( "\t[1] : RESOLUTION DE F(x) = 0" )
+print( "\t[2] : RESOLUTION DE AX = B" )
+print( "\t[3] : INTERPOLATION LINEAIRE" )
+print( "\t[4] : EQUATION DIFFERENTIELLE\n" )
+
+while True:
+    try:
+        choix = input( "Votre choix : " )
+        choix = int( choix )
+        if ( 1 <= choix & choix <= 4 ):
+            break
+        else:
+            print( "Veuillez entrer un choix valide" )
+    except:
+        print( "Veuillez entrer un chiffre" )
